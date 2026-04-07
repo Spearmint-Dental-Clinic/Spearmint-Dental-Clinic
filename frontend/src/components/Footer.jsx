1400e7212e43d52404ec3011069f6436506430e3
Hannah-Muthoni-Hailu <muthonihannahhailu@gmail.com>
2026-04-07 10:59:26 +0300

    Footer responsiveness completed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,17 +4,19 @@ import logo from '../assets/logo.png';
 export default function Footer() {
     return(
         <Container id="footer" className="p-3 border-top mt-5">
-            <Row>
-                <Col>
-                    <h4 style={{ marginLeft: "90px", marginTop: "20px" }}>
-                        <Image src={logo} alt="Team member image" style={{ width: "50px" }} roundedCircle></Image>
+            <Row className="text-center text-md-start align-items-center">
+                <Col xs={12} md={6} className="mb-4 mb-md-0">
+                    <h4>
+                        <Image src={logo} alt="Logo" style={{ width: "50px" }} roundedCircle className="me-2" />
                         <span className="headerBrandA">Spearmint</span> <span className="headerBrandB">Dental Clinic</span>
                     </h4>
                 </Col>
-                <Col>
-                    <Nav.Link href="tel:+254701561597"><small>(254) 701-561-597</small></Nav.Link>
-                    <Nav.Link href="tel:+254745382230"><small>(254) 745-382-230</small></Nav.Link>
-                    <Nav.Link href="mailto:spearmintclinic@gmail.com"><small>spearmintclinic@gmail.com</small></Nav.Link>
+                <Col xs={12} md={6}>
+                    <div className="d-flex flex-column align-items-center align-items-md-end">
+                        <Nav.Link href="tel:+254701561597" className="p-1"><small>(254) 701-561-597</small></Nav.Link>
+                        <Nav.Link href="tel:+254745382230" className="p-1"><small>(254) 745-382-230</small></Nav.Link>
+                        <Nav.Link href="mailto:spearmintclinic@gmail.com" className="p-1"><small>spearmintclinic@gmail.com</small></Nav.Link>
+                    </div>
                 </Col>
             </Row>
         </Container>
