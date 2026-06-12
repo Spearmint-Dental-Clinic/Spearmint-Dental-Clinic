8311aafda54e247763ce9b4f469af023aa0cb608
Hannah Hailu Muthoni <muthonihannahhailu@gmail.com>
2026-06-12 19:14:35 +0300

    Changed parent container width

--- frontend/src/components/TeamItems.jsx
+++ frontend/src/components/TeamItems.jsx
@@ -3,9 +3,9 @@ import { Image, Container, Row, Col } from "react-bootstrap"
 export default function TeamItems({ image, name, description }) {
     return(
         <Container className="teamItem rounded p-3 mt-5 w-75 shadow-lg">
-            <Row>
+            <Row className="w-50">
                 <Col xs={5} lg={3} className="mb-3">
-                    <Image src={image} alt="Team member image" className="w-100 h-100" roundedCircle></Image>
+                    <Image src={image} alt="Team member image" className="w-100" roundedCircle></Image>
                 </Col>
                 <Col>  
                     <h4>{name}</h4>
