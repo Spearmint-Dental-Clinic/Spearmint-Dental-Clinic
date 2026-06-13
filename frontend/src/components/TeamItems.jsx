887bf62e64e246792c81ff0e17f99bcaa6c8963b
Hannah Hailu Muthoni <muthonihannahhailu@gmail.com>
2026-06-13 13:23:32 +0300

    Changed the margins and width of the image for mobile phones

--- frontend/src/components/TeamItems.jsx
+++ frontend/src/components/TeamItems.jsx
@@ -4,7 +4,7 @@ export default function TeamItems({ image, name, description }) {
     return(
         <Container className="teamItem rounded p-3 mt-5 w-75 shadow-lg">
             <Row className="w-75 mx-auto">
-                <Col xs={5} lg={3} className="mb-3">
+                <Col xs={6} lg={3} className="mb-3 mx-auto">
                     <Image src={image} alt="Team member image" className="w-100" roundedCircle></Image>
                 </Col>
                 <Col className="pt-3">  
