bb596f5fd3912ccf44c02f0d5f19cef3a89f6f85
Hannah Hailu Muthoni <muthonihannahhailu@gmail.com>
2026-06-13 13:27:22 +0300

    Removed bottom border from team image

--- frontend/src/components/TeamItems.jsx
+++ frontend/src/components/TeamItems.jsx
@@ -4,8 +4,8 @@ export default function TeamItems({ image, name, description }) {
     return(
         <Container className="teamItem rounded p-3 mt-5 w-75 shadow-lg">
             <Row className="w-75 mx-auto">
-                <Col xs={6} lg={3} className="mb-3 mx-auto">
-                    <Image src={image} alt="Team member image" className="w-100 border border-1" roundedCircle></Image>
+                <Col xs={6} lg={3} className="mx-auto">
+                    <Image src={image} alt="Team member image" className="w-100 border border-2" roundedCircle></Image>
                 </Col>
                 <Col className="pt-3">  
                     <h4>{name}</h4>
